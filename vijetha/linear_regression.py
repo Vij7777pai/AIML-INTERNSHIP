30a040ec72645c526def8db505010be564012be5
Vijetha pai <vijethapai1234@gmail.com>
2023-09-07 10:38:35 +0530

    aiml

--- vijetha/linear_regression.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn import linear_model
-import matplotlib.pyplot as plt
-data=pd.read_csv('insurance.csv')
-dfl=pd.DataFrame(data)
-print(dfl)
-dfl.isnull()
